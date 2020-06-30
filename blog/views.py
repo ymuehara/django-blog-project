1b2eaa3e6e0868262840f54a810bc687e06da730
Yuri Uehara <yuriuehara@hotmail.com>
2020-06-30 17:40:21 -0300

    Atualizacao projeto Django depois do video 3

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'YuriU',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 29th, 2020'
+    },
+    {
+        'author': 'PancakeU',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 30th, 2020'
+    }
+]
+
 # function returns what the user will see when sent this route
 
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+# return HttpResponse("<h1>Blog Home</h1>")
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html', {'title': 'About'})
