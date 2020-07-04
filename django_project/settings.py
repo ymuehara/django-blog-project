b91631583b7feb909d197ca141063814fae69d2d
Yuri Uehara <yuriuehara@hotmail.com>
2020-07-04 18:17:32 -0300

    atualizacao projeto django corey apos video 6 user registration

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms', #third party django application pip install django-crispy-forms. Crispy forms allows us to put tags in our template that will style our forms in a bootstrap fashion
+
 ]
 
 MIDDLEWARE = [
@@ -119,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4' #because default is 2 but it is old
